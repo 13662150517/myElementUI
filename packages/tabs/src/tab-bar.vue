6ab14f84b37862615ecece77e93f0aad44a0ea42
dongliangfu <13662150517@163.com>
2019-01-04 10:07:14 +0800

    更新

--- packages/tabs/src/tab-bar.vue
+++ packages/tabs/src/tab-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="el-tabs__active-bar" :class="`is-${ rootTabs.tabPosition }`" :style="barStyle"></div>
+  <div class="el-tabs__active-bar" :class="`is-${ rootTabs.tabPosition }`" :style="barStyle()"></div>
 </template>
 <script>
   import { arrayFind } from 'element-ui/src/utils/util';
@@ -12,46 +12,44 @@
 
     inject: ['rootTabs'],
 
-    computed: {
-      barStyle: {
-        get() {
-          const tabs = this.tabs;
-          if (!tabs || !this.$parent.$refs.tabs) return {};
-          let style = {};
-          let offset = 0;
-          let tabSize = 0;
-          const sizeName = ['top', 'bottom'].indexOf(this.rootTabs.tabPosition) !== -1 ? 'width' : 'height';
-          const sizeDir = sizeName === 'width' ? 'x' : 'y';
-          const firstUpperCase = str => {
-            return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
-          };
-          tabs.every((tab, index) => {
-            let $el = arrayFind(this.$parent.$refs.tabs, t => t.id.replace('tab-', '') === tab.paneName);
-            if (!$el) { return false; }
+    methods: {
+      barStyle() {
+        const tabs = this.tabs;
+        if (!tabs || !this.$parent.$refs.tabs) return {};
+        let style = {};
+        let offset = 0;
+        let tabSize = 0;
+        const sizeName = ['top', 'bottom'].indexOf(this.rootTabs.tabPosition) !== -1 ? 'width' : 'height';
+        const sizeDir = sizeName === 'width' ? 'x' : 'y';
+        const firstUpperCase = str => {
+          return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+        };
+        tabs.every((tab, index) => {
+          let $el = arrayFind(this.$parent.$refs.tabs, t => t.id.replace('tab-', '') === tab.paneName);
+          if (!$el) { return false; }
 
-            if (!tab.active) {
-              offset += $el[`client${firstUpperCase(sizeName)}`];
-              return true;
-            } else {
-              tabSize = $el[`client${firstUpperCase(sizeName)}`];
-              if (sizeName === 'width' && tabs.length > 1) {
-                tabSize -= (index === 0 || index === tabs.length - 1) ? 20 : 40;
-              }
-              return false;
+          if (!tab.active) {
+            offset += $el[`client${firstUpperCase(sizeName)}`];
+            return true;
+          } else {
+            tabSize = $el[`client${firstUpperCase(sizeName)}`];
+            if (sizeName === 'width' && tabs.length > 1) {
+              tabSize -= (index === 0 || index === tabs.length - 1) ? 20 : 40;
             }
-          });
-
-          if (sizeName === 'width' && offset !== 0) {
-            offset += 20;
+            return false;
           }
-          const transform = `translate${firstUpperCase(sizeDir)}(${offset}px)`;
-          style[sizeName] = tabSize + 'px';
-          style.transform = transform;
-          style.msTransform = transform;
-          style.webkitTransform = transform;
+        });
 
-          return style;
+        if (sizeName === 'width' && offset !== 0) {
+          offset += 20;
         }
+        const transform = `translate${firstUpperCase(sizeDir)}(${offset}px)`;
+        style[sizeName] = tabSize + 'px';
+        style.transform = transform;
+        style.msTransform = transform;
+        style.webkitTransform = transform;
+
+        return style;
       }
     }
   };
