dacf5f23daa07c22b242e353058489f6ddf46d2c
dongliangfu <13662150517@163.com>
2019-05-08 15:13:20 +0800

    升级文档

--- src/utils/util.js
+++ src/utils/util.js
@@ -139,3 +139,18 @@ export const getRandomId = () => {
   var uuid = s.join('');
   return uuid;
 };
+
+export const autoprefixer = function(style) {
+  if (typeof style !== 'object') return style;
+  const rules = ['transform', 'transition', 'animation'];
+  const prefixes = ['ms-', 'webkit-'];
+  rules.forEach(rule => {
+    const value = style[rule];
+    if (rule && value) {
+      prefixes.forEach(prefix => {
+        style[prefix + rule] = value;
+      });
+    }
+  });
+  return style;
+};
