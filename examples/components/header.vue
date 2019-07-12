f2ae38ed2fed3fed08c95a747bfdfafa21387053
dongliangfu <13662150517@163.com>
2019-07-12 09:15:04 +0800

    更新版本2.9.0

--- examples/components/header.vue
+++ examples/components/header.vue
@@ -268,8 +268,7 @@
             </router-link>
           </li>
           <li 
-            class="nav-item"
-            v-if="$isEle"
+            class="nav-item nav-item-theme"
           >
             <router-link
               active-class="active"
@@ -311,10 +310,6 @@
                 </el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
-          </li>
-          <!--theme picker-->
-          <li class="nav-item nav-theme-switch" v-show="isComponentPage">
-            <theme-picker v-if="!$isEle"></theme-picker>
           </li>
         </ul>
       </div>
